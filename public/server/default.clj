2da03dfa9e1c5b4bef28647e804a709b5951f796
Wildhoney <adam.timberlake@gmail.com>
2015-10-07 22:30:16 +0100

    Modified server

---
+++ public/server/default.clj
@@ -0,0 +1,28 @@
+(ns maptalk.handler
+  (:use compojure.core)
+  (:require [clj-json.core :as json]))
+
+(ns server
+  (:use ring.adapter.jetty
+        ring.middleware.file
+        ring.middleware.file-info
+        ring.middleware.format-response
+        [ring.util.response :exclude (not-found)]
+        compojure.core
+        compojure.route
+  ))
+
+(defroutes handler
+  (GET "/" [] (file-response "default.html" {:root "html"}))
+  (GET "/api/echo" request
+       {:status 200
+        :body (dissoc request :body)})
+  (files "" {:root "html"})
+  (not-found "<h1>404 Page not found</h1>")
+)
+
+(def app (-> handler
+             wrap-clojure-response))
+
+#_(defonce server
+  (run-jetty #'app {:port 3000 :join? false}))
